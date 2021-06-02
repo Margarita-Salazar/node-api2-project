897bfff36b426fd282b069a9209a8bb26d8dfa60
unknown <margarita.salazar.mls@gmail.com>
2021-06-02 00:42:46 -0500

    endpoint for updating post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -54,8 +54,36 @@ router.post('/', async (req, res)=>{
     }
  
 })
-router.put('/:id', (req, res)=>{
-    
+router.put('/:id', async (req, res)=>{
+    const id = req.params.id
+    const { title, contents } = req.body
+    const post = await Post.findById(id)
+    try{
+        if(post){
+            if(
+                !title ||
+                !title.trim() ||
+                !contents ||
+                !contents.trim()
+            ){
+                res.status(400).json({
+                    message: 'Please provide title and contents for the post'
+                })
+            }else{
+                await Post.update(id, req.body)
+                const updatePost = await Post.findById(id)
+                res.status(200).json(updatePost)
+            }
+        }else{
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+        }
+    }catch(err){
+        res.status(500).json({
+            message: 'The post information could not be modified'
+        })
+    }
 })
 router.delete('/:id', (req, res)=>{
 
