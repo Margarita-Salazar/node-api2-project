16978ed280c60bc501955e283b39b0060c88a7d6
unknown <margarita.salazar.mls@gmail.com>
2021-06-02 00:12:40 -0500

    added endpoint to retreive all pots

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,4 +1,31 @@
 // implement your posts router here
 const router = require('express').Router()
 
+const Post = require('./posts-model')
+
+router.get('/', (req, res)=>{
+    Post.find()
+        .then(post =>{
+            res.status(200).json(post)
+        })
+        .catch(()=>{
+            res.status(500).json({
+                message: 'The posts information could not be retrieved'
+            })
+        })
+})
+router.get('/:id', (req, res)=>{
+    
+})
+router.post('/', (req, res)=>{
+    
+})
+router.put('/:id', (req, res)=>{
+    
+})
+router.delete('/:id', (req, res)=>{
+
+})
+
+
 module.exports = router
