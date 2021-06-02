fca16f741abec0e74af03162801e2ba0d00dc9e0
unknown <margarita.salazar.mls@gmail.com>
2021-06-02 00:19:59 -0500

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -14,11 +14,24 @@ router.get('/', (req, res)=>{
             })
         })
 })
-router.get('/:id', (req, res)=>{
-    
+router.get('/:id', async (req, res)=>{
+    const post = await Post.findById(req.params.id)
+    try{
+        if(post){
+            res.status(200).json(post)
+        }else{
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+        }
+    }catch(err){
+        res.status(500).json({
+            message: 'The post information could not be retrieved'
+        })
+    }
 })
 router.post('/', (req, res)=>{
-    
+ 
 })
 router.put('/:id', (req, res)=>{
     
